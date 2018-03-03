7135820042db2c146709752ac84aec599d5d3a50
zhuo95 <34971718+zhuo95@users.noreply.github.com>
2018-03-03 12:59:01 -0500

    Add I/O instruction and I/Omemory

---
+++ ComputerSimulator/src/core/IOmemory.java
@@ -0,0 +1,44 @@
+package core;
+
+import java.util.HashMap;
+//I/O mapped I/O it has separated i/o memory space
+public class IOmemory {
+    //use HashMap to represent IOmemory,key represents address,value represents data
+    private HashMap<String ,String> IOmemorySpace=new HashMap<String, String>();
+    //size of the IOmemory
+    private final int size = 32;
+    //Single instance of IOMemory
+    private static final IOmemory instance = new IOmemory();
+
+    //constructor
+    private IOmemory(){
+        String address="00000";
+        for(int i=0;i<size;i++){
+            IOmemorySpace.put(address,"0000000000000000");
+            address=Memory.getInstance().addressAddone(address);
+        }
+    }
+    //Method to get instance
+    public static IOmemory getInstance() {
+        return instance;
+    }
+
+    //get the content from  memory
+    public String getContent(String address){
+        if(address.length()==5){
+            return IOmemorySpace.get(address);
+        }else {
+            return "Error";
+        }
+    }
+
+    //store value to the address
+    public void setContent(String address,String value){
+       if(address.length()==5){
+           IOmemorySpace.put(address,value);
+        }else {
+            System.err.println("Illegal address length!!");
+        }
+
+    }
+}
